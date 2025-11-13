7ada442d6f69b42174f8f97a89e3f03880811b78
Jettsyy <teazyyy2512@gmail.com>
2025-11-13 17:52:45 +0700

    feat: mutate create product

--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { use } from "react";
 import { Button, Form, Input, Select } from "antd";
+import type { IProduct } from "../pages/Product";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useNavigate } from "react-router";
 
 const CreateProduct = () => {
+  const [form] = Form.useForm();
+  const navigate = useNavigate();
+
+  const createProduct = async (payload: IProduct) => {
+    const { data } = await axios.post(
+      "http://localhost:3000/products",
+      payload
+    );
+    return data;
+  };
+  const mutation = useMutation({
+    mutationFn: createProduct,
+    onSuccess: () => {
+      navigate("/product");
+    },
+  });
+  const handleCreate = (value: IProduct) => {
+    mutation.mutate(value);
+  };
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>Create Product</h1>
-      <Form layout="vertical">
+      <Form layout="vertical" form={form} onFinish={handleCreate}>
         <Form.Item
           name="name"
           label="Name"
